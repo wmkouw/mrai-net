7ae85c48de0d6ab7f156ad2068a612da4125ea39
Wouter Kouw <wmkouw@gmail.com>
2018-07-15 23:08:05 +0200

    Added one more test for util

--- mrai/tests/test_util.py
+++ mrai/tests/test_util.py
@@ -3,3 +3,10 @@ import numpy.random as rn
 
 from mrai.util import strip_skull
 
+
+def test_strip_skull():
+    I = rn.randn(1, 3, 3)
+    M = np.concatenate((np.full((1, 3, 1), True),
+                        np.full((1, 3, 2), False)), axis=2)
+    J = strip_skull(I, M)
+    assert np.all(J[0, :, 0] == 0)
