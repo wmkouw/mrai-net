425cc2953b4052245df996009bd6b1333233b453
Wouter Kouw <wmkouw@gmail.com>
2018-07-24 16:53:16 +0200

    another attempt at getting readthedocs to autodoc

--- mrainet/docs/conf.py
+++ mrainet/docs/conf.py
@@ -31,7 +31,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autosummary',
+              'sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'numpydoc']
 
